d6b0b5b9c55091372ea786ca437cb731613ea0b7
LeoKens-tofu <dattranthanhtien05@gmail.com>
2025-08-19 23:16:33 +0700

    apply register

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -2,3 +2,9 @@ module.exports.login = (req, res) => {
   res.render('admin/pages/login', {
     title: 'Admin Login',});
 }
+
+module.exports.register = (req, res) => {
+  res.render('admin/pages/register',{
+    title: 'Admin Register'
+  })
+}
