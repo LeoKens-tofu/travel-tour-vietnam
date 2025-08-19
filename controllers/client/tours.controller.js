0336529c040860a0061c6a0590637dfdf5c15362
LeoKens-tofu <dattranthanhtien05@gmail.com>
2025-08-19 21:13:52 +0700

    apply home page

--- controllers/client/tours.controller.js
+++ controllers/client/tours.controller.js
@@ -3,6 +3,7 @@ const Tour = require('../../models/tours.model')
 module.exports.tour = async (req, res) => {
     const tourList = await Tour.find({});
     res.render('client/pages/list', {
+        title: 'Tour List',
         tourList: tourList
     });
 }
