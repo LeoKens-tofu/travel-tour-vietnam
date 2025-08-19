e44abfa16baf3741dfd03fad3e864280529b83d7
LeoKens-tofu <dattranthanhtien05@gmail.com>
2025-08-19 22:29:49 +0700

    apply cart page

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoute = require('./home.route');
 const tourRoute = require('./tour.route');
+const cartRoute = require('./cart.route');
 router.use('/', homeRoute);
 router.use('/tours', tourRoute);
+router.use('/cart', cartRoute);
 module.exports = router;
