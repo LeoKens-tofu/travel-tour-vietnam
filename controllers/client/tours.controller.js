dd94d1a1592569ad10bd311c7593fd15f255a68f
LeoKens-tofu <dattranthanhtien05@gmail.com>
2025-10-20 14:04:39 +0700

    update

--- controllers/client/tours.controller.js
+++ controllers/client/tours.controller.js
@@ -1,15 +1,43 @@
-const Tour = require('../../models/tours.model')
-
-module.exports.tour = async (req, res) => {
-    const tourList = await Tour.find({});
-    res.render('client/pages/list', {
-        title: 'Tour List',
-        tourList: tourList
-    });
-}
+const Tour = require("../../models/tours.model");
+const Category = require("../../models/category.model");
 
 module.exports.detail = async (req, res) => {
-    res.render('client/pages/tour-detail', {
-        title: 'Tour Detail',
+  const slug = req.params.slug;
+
+  const tourDetail = await Tour.findOne({
+    slug: slug,
+    deleted: false,
+    status: "active",
+  });
+
+//   if (!tourDetail) {
+//     res.redirect("/");
+//   }
+
+  const breadcrumb = [];
+  if (tourDetail.category) {
+    const category = await Category.findOne({
+      _id: tourDetail.category,
+      deleted: false,
+      status: "active",
     });
-}
+
+    if (category) {
+      breadcrumb.push({
+        slug: category.slug,
+        name: category.name,
+      });
+    }
+  }
+
+  breadcrumb.push({
+    slug: tourDetail.slug,
+    name: tourDetail.name,
+    avatar: tourDetail.avatar,
+  });
+
+  res.render("client/pages/tour-detail", {
+    title: "Tour Detail",
+    breadcrumb :breadcrumb,
+  });
+};
--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const tourController = require('../../controllers/client/tours.controller')
-router.get('/', tourController.tour)
-router.get('/detail', tourController.detail)
+
+router.get('/detail/:slug', tourController.detail)
+
 module.exports = router
